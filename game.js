c96ddd0d6f0b8cc94149f51b40cc4a77d8994e88
David Sima <davidsima@windowslive.com>
2019-02-15 11:47:44 +0200

    Adding new images

--- game.js
+++ game.js
@@ -1,44 +1,24 @@
 // ⚫ the idea it is to create a Card memory game without using any library
 // ⚫ I'll aproach this challenge with a 4x4 grid => a total of 16 cards with 8 unique cards
-// ⚫ I've decided to use RoboHash for card images creating unique images from the index
 
 const gameHolder = document.getElementById("game")
+const water = document.getElementById("water-level")
 
-const image = (hash, set) => `https://robohash.org/${hash}.jpg?size=150x150&set=set${set}`
-
-const generateCard = (imgLink) => `<div class="card back" onclick="check(event)"><img onload="imageLoaded()" onerror="cantLoad()" src=${imgLink} /></div>`
+const generateCard = imgLink => `<div class="card back" onclick="check(event)"><img onload="imageLoaded()" onerror="cantLoad()" src=${imgLink} /></div>`
 
 const state = []
-let currentlyLoading
-
-const initiateLoading = () => {
-    currentlyLoading = 16
-    gameHolder.classList.add("loading")
-}
 
 const cleanState = () => {while(state.length) state.pop()}
 
-const startGame = (set) => {
+const startGame = () => {
     gameHolder.innerHTML = ''
     cleanState()
-    const uniqueCards =  Array.apply(null, Array(8)).map( (card, index) => generateCard( image(index, set) ) )
+    const uniqueCards =  Array.apply(null, Array(8)).map( (card, index) => generateCard( `images/${index}.svg` ) )
     const cards = uniqueCards.concat(uniqueCards)
     cards.sort( () => Math.random() > Math.random() )
     cards.forEach(card => {
         gameHolder.innerHTML += card
     })
-    initiateLoading()
-}
-
-const imageLoaded = () => {
-    currentlyLoading--
-    if(!currentlyLoading)
-        gameHolder.classList.remove("loading")
-}
-
-const cantLoad = () => {
-    gameHolder.classList.remove("loading")
-    gameHolder.classList.add("error")
 }
 
 const endGame = () => {
@@ -47,30 +27,34 @@ const endGame = () => {
 
 let foundPairs = 0
 let clicks = 0
+let waterLevel = Number(getComputedStyle(water).getPropertyValue("--size").replace("px", ""))
 
 const check = (event) => {
-    clicks++
-    event.target.classList.replace("back", "front")
-    state.push({
-        card: event.target,
-        imgSrc: event.target.firstChild.currentSrc
-    })
-    if(state.length===2)
-    {
-        if(state[0].imgSrc===state[1].imgSrc) {
-            foundPairs++
-            cleanState()
-            if(foundPairs===8) endGame()
-        }
-        else {
-            window.setTimeout(()=>{
-                state.forEach(el => {
-                    el.card.classList.replace("front", "back")
-                })
+    if (state.length < 2) {
+        clicks++
+        event.target.classList.replace("back", "front")
+        state.push({
+            card: event.target,
+            imgSrc: event.target.firstChild.currentSrc
+        })
+        if (state.length === 2) {
+            if (state[0].imgSrc === state[1].imgSrc) {
+                foundPairs++
                 cleanState()
-            },800)
+                if (foundPairs === 8) endGame()
+            }
+            else {
+                waterLevel += 25
+                water.style = `--size: ${waterLevel}px;`
+                window.setTimeout(() => {
+                    state.forEach(el => {
+                        el.card.classList.replace("front", "back")
+                    })
+                    cleanState()
+                }, 800)
+            }
         }
     }
 }
 
-startGame(4)
+startGame()
