527e51b02f170da246398850fc0b8540ea86786f
David Sima <davidsima@windowslive.com>
2019-02-15 16:38:20 +0200

    Wave animation and sounds

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
+/* eslint-disable init-declarations */
 // ⚫ the idea it is to create a Card memory game without using any library
 // ⚫ I'll aproach this challenge with a 4x4 grid => a total of 16 cards with 8 unique cards
 
 const gameHolder = document.getElementById("game")
-const water = document.getElementById("water-level")
+const water = document.getElementById("water")
+const drop = document.getElementById("drop")
+const wrong = document.getElementById("wrong")
+const clickContainer = document.getElementById("clicks")
+const timeContainer = document.getElementById("time")
 
 const generateCard = imgLink => `<div class="card back" onclick="check(event)"><img onload="imageLoaded()" onerror="cantLoad()" src=${imgLink} /></div>`
 
@@ -20,6 +25,12 @@ const startGame = () => {
         gameHolder.innerHTML += card
     })
 }
+const startTime = () => {
+    let seconds = 0;
+    window.setInterval(() => {
+        timeContainer.innerHTML = ++seconds
+    }, 1000)
+}
 
 const endGame = () => {
     gameHolder.classList.add("end")
@@ -29,9 +40,13 @@ let foundPairs = 0
 let clicks = 0
 let waterLevel = Number(getComputedStyle(water).getPropertyValue("--size").replace("px", ""))
 
+// eslint-disable-next-line no-unused-vars
 const check = (event) => {
+    if(!clicks) startTime()
     if (state.length < 2) {
-        clicks++
+        drop.currentTime = 0
+        drop.play()
+        clickContainer.innerHTML = ++clicks
         event.target.classList.replace("back", "front")
         state.push({
             card: event.target,
@@ -44,7 +59,9 @@ const check = (event) => {
                 if (foundPairs === 8) endGame()
             }
             else {
-                waterLevel += 25
+                wrong.currentTime = 0
+                wrong.play()
+                waterLevel += 50
                 water.style = `--size: ${waterLevel}px;`
                 window.setTimeout(() => {
                     state.forEach(el => {
