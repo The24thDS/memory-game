4ace516539e2c05e6c58cae984f4da0ad65e6ad2
David <davidsima@windowslive.com>
2019-02-22 20:59:11 +0200

    CORS

--- tracking.js
+++ tracking.js
@@ -1,22 +1,23 @@
 const tracking = true
 const matomo = 'https://david.mywire.org/piwik/'
+const headers = new Headers({'Access-Control-Allow-Origin': 'https://the24thds.github.io/memory-game/'})
 
 const optOut = async () => {
-    const response = await fetch(`${matomo}index.php?module=API&method=AjaxOptOut.doIgnore`)
+    const response = await fetch(`${matomo}index.php?module=API&method=AjaxOptOut.doIgnore`, {headers})
     const text = await response.text()
     const data = new window.DOMParser().parseFromString(text, "text/xml")
     console.log(data)
 }
 
 const checkStatus = async () => {
-    const response = await fetch(`${matomo}index.php?module=API&method=AjaxOptOut.isTracked`)
+    const response = await fetch(`${matomo}index.php?module=API&method=AjaxOptOut.isTracked`, {headers})
     const text = await response.text()
     const data = new window.DOMParser().parseFromString(text, "text/xml")
     console.log(data)
 }
 
 const optIn = async () => {
-    const response = await fetch(`${matomo}index.php?module=API&method=AjaxOptOut.doTrack`)
+    const response = await fetch(`${matomo}index.php?module=API&method=AjaxOptOut.doTrack`, {headers})
     const text = await response.text()
     const data = new window.DOMParser().parseFromString(text, "text/xml")
     console.log(data)
